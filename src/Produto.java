4a430661b0f8bc957b0566119456bab6d87aa926
Gustavo Duarte <donhaduarte@gmail.com>
2024-02-29 16:56:20 -0300

    class Produto

---
+++ src/Produto.java
@@ -0,0 +1,38 @@
+public class Produto {
+    private String nome;
+    private double preco;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+
+    public void aplicarDesconto(double percentual){
+        double desconto = preco * (percentual / 100);
+        preco -= desconto;
+    }
+}
+
+
+public class Principal{
+    public static void main(String[]args){
+        Produto produto = new Produto("Celular", 2000.0);
+
+        System.out.println("Nome do Produto: " + produto.getNome());
+        System.out.println("Preço: " + produto.getPreco());
+
+        produto.aplicarDesconto(10);
+        System.out.println("Novo Preço após Desconto: " + produto.getPreco());
+    }
+}
