6df34d9f05de857d2bffda5ccf4865bc4039efc4
Gustavo Duarte <donhaduarte@gmail.com>
2024-02-29 16:38:04 -0300

    new class

---
+++ src/Aluno.java
@@ -0,0 +1,19 @@
+public class Aluno {
+    String nome;
+    int idade;
+
+    void exibeInformacoes(){
+        System.out.println("Nome do aluno: " + nome);
+        System.out.println("Idade do aluno: " + idade);
+    }
+}
+
+public class Principal{
+    public static void main(String[] args) {
+        Aluno meuAluno = new Aluno();
+        meuAluno.nome = "Akemi";
+        meuAluno.idade = 18;
+
+        meuAluno.exibeInformacoes();
+    }
+}
--- src/Carro.java
+++ src/Carro.java
@@ -1,3 +1,27 @@
 public class Carro {
+    String modelo;
+    int ano;
+    String cor;
 
+    void exibeFichaTecnica(){
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+        System.out.println("Cor: " + cor);
+    }
+    int calculaIdade(){
+        return  2023 - ano;
+    }
+}
+
+public class Principal{
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro();
+        meuCarro.modelo = "Gol";
+        meuCarro.ano = 2023;
+        meuCarro.cor = "Preto";
+
+        meuCarro.exibeFichaTecnica();
+
+        System.out.println("Idade do carro: " + meuCarro.calculaIdade() + "anos");
+    }
 }
