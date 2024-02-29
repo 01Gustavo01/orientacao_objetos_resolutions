81a416e5b1697c3cac0d787e7b5cd9ef71d69f15
Gustavo Duarte <donhaduarte@gmail.com>
2024-02-29 19:12:50 -0300

    Uso interface, @ Override, implements

---
+++ src/ConversorTemperatura.java
@@ -0,0 +1,33 @@
+public interface ConversorTemperatura {
+    double celsiusParaFahrenheit(double temperaturaCelsius);
+    double fahrenheitParaCelsius(double temperaturaFahrenheit);
+}
+
+public class ConversorTemperaturaPadrao implements ConversorTemperatura{
+    @Override
+    public double celsiusParaFahrenheit(double temperaturaCelsius){
+        return (temperaturaCelsius * 9/5) + 32;
+    }
+
+    @Override
+    public double fahrenheitParaCelsius(double temperaturaFahrenheit){
+        return(temperaturaFahrenheit - 32) * 5/9;
+    }
+}
+
+
+
+//Teste
+public class TesteConversorTemperatura{
+    public static void main(String[]args){
+        ConversorTemperatura conversor = new ConversorTemperatura();
+
+        double temperaturaCelsius = 25;
+        double temperaturaFahrenheit = conversor.celsiusParaFahrenheit(temperaturaCelsius);
+        System.out.println(temperaturaCelsius + " Celsius é igual a " + temperaturaFahrenheit + " Fahrenheit.");
+
+        temperaturaFahrenheit = 77;
+        temperaturaCelsius = conversor.fahrenheitParaCelsius(temperaturaFahrenheit);
+        System.out.println(temperaturaFahrenheit + " Fahrenheit é igual a " + temperaturaCelsius + " Celsius.");
+    }
+}
